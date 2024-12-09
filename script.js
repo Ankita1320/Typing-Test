37fbf601baae0814db3a93825f6fdf1370d5048a
ankita1320 <ank1020ita@gmail.com>
2024-12-10 00:33:09 +0530

    first commit

--- script.js
+++ script.js
@@ -16,8 +16,7 @@ let isTyping = false;
 
 
 function loadParagraph(){
-    const paragraph= [ " Avoid daydreaming about the years to come.","You are the most important person in your whole life.","Always be true to who you are, and ignore what other people have to say about you.","Always be true to who you are, and ignore what other people have to say about you.","Only demonstrate your strength when it’s really required.","Subscribe to Drop X Out"
-];
+    const paragraph= [ " Avoid daydreaming about the years to come.","You are the most important person in your whole life.","Always be true to who you are, and ignore what other people have to say about you.","Always be true to who you are, and ignore what other people have to say about you.","Only demonstrate your strength when it’s really required."];
 
 const randomIndex = Math.floor(Math.random()*paragraph.length);
 typingText.innerHTML='';
